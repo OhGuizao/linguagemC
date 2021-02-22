82a416d9ecf7c60b63f6ad9323754678d08fa88e
OhGuizao <79462445+OhGuizao@users.noreply.github.com>
2021-02-22 11:44:35 -0300

    adicionei aula 1

---
+++ Aula 1/Hello/heloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* 
+	Aula 1  -  Hello word
+	@author Guilherme Vieira 
+*/
+int main(int argc, char** argv) {
+	printf("Hello World\n");
+	// a linha abaixo gera uma pausa no prompt do MS-DOS
+	system("pause");
+	return 0;
+}
+
